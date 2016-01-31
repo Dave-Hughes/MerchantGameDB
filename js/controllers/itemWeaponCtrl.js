699acf3b5e5a49944a7fc2d6e07d028eb36daf23
David Hughes <davehughesonline@gmail.com>
2016-01-31 15:54:25 -0700

    Changes to main.

--- js/controllers/itemWeaponCtrl.js
+++ js/controllers/itemWeaponCtrl.js
@@ -6,22 +6,73 @@ angular.module('mainApp')
 			$(this).toggleClass( "filter-selected" )
 	  })
 
+		//Blacksmith option click handler
 	  $("#blacksmith-option").on("click", function() {
 			$("#bs-weapons-subtypes").toggleClass("hide");
+
+			//If blacksmith option deselected
+			if (!$(this).hasClass("filter-selected")) {
+				//Reset BS weapon subtypes
+				$scope.Filter.Sword							= false;
+				$scope.Filter.Longsword					= false;
+				$scope.Filter.Axe								= false;
+				$scope.Filter.Hatchet						= false;
+				$scope.Filter.Dagger						= false;
+				$scope.Filter.Knife							= false;
+
+				//Remove "filter-selected" from BS weapon subtypes
+				$("#bs-weapons-subtypes label").removeClass("filter-selected");
+
+				$scope.$apply();
+			}
 	  })
 
-	  $("#woodworker-option").on("click", function() {
+		//Woodworker Option Click Handled
+		$("#woodworker-option").on("click", function() {
 			$("#ww-weapons-subtypes").toggleClass("hide");
+
+			//If blacksmith option deselected
+			if (!$(this).hasClass("filter-selected")) {
+				//Reset WW weapon subtypes
+				$scope.Filter.Wand							= false;
+				$scope.Filter.MysticWand				= false;
+				$scope.Filter.Stave							= false;
+				$scope.Filter.MysticStave				= false;
+				$scope.Filter.Cudgel						= false;
+				$scope.Filter.Club							= false;
+
+				//Remove "filter-selected" from WW weapon subtypes
+				$("#ww-weapons-subtypes label").removeClass("filter-selected");
+
+				$scope.$apply();
+			}
 	  })
 
 		//Reset button functionality
-		$(".filters-reset-button").on("click", function() {
-			console.log("running");
+		$(".filters-reset-button").on("click", function(item) {
+			$scope.Filter.Tier1							= false;
+			$scope.Filter.Tier2							= false;
+			$scope.Filter.Tier3							= false;
+			$scope.Filter.Tier4							= false;
+			$scope.Filter.Tier5							= false;
+			$scope.Filter.Sword							= false;
+			$scope.Filter.Longsword					= false;
+			$scope.Filter.Axe								= false;
+			$scope.Filter.Hatchet						= false;
+			$scope.Filter.Dagger						= false;
+			$scope.Filter.Knife							= false;
+			$scope.Filter.Wand							= false;
+			$scope.Filter.MysticWand				= false;
+			$scope.Filter.Stave							= false;
+			$scope.Filter.MysticStave				= false;
+			$scope.Filter.Cudgel						= false;
+			$scope.Filter.Club							= false;
+			$scope.$apply();
 			$(".filter-selected").removeClass("filter-selected");
 			$("#bs-weapons-subtypes").addClass("hide");
 			$("#ww-weapons-subtypes").addClass("hide");
-			$scope.showWeaponsByType();
 	  })
+
 
 	  $scope.equipment = jsonEquipments;
 	  $scope.Filter = "";
