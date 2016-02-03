1ef145c17828ceb1c792b61347168f780083e882
Phaturia <phaturia@gmail.com>
2016-02-04 00:33:39 +0100

    Revert "itemArmor"
    This reverts commit 5cba1d98fe9d2fc5345a08cfc61cff9b068b2ed4.

--- js/controllers/itemArmorCtrl.js
+++ js/controllers/itemArmorCtrl.js
@@ -15,7 +15,7 @@ angular.module('mainApp')
 
 	  $scope.equipment = jsonEquipments;
 	  $scope.formula = jsonFormulas;
-	  $scope.Filter = {};
+	  $scope.Filter = "";
 	  $scope.showArmorsByType = function(item){
 		return 		item.subType === $scope.Filter.Bracers ||
 					item.subType === $scope.Filter.Greaves ||
@@ -43,93 +43,33 @@ angular.module('mainApp')
 					  lvToTier(item.itemLevel) == $scope.Filter.Tier4 ||
 					  lvToTier(item.itemLevel) == $scope.Filter.Tier5;
 	  };
-		
-		//toggle select all tier
-		var isAllTierOn = false;
-		$scope.selectTier = function(){
-			if(isAllTierOn)
-				{
-				isAllTierOn = false;
-				$scope.Filter.Tier1			= false;
-				$scope.Filter.Tier2			= false;
-				$scope.Filter.Tier3			= false;
-				$scope.Filter.Tier4			= false;
-				$scope.Filter.Tier5			= false;
-				$(".armors-tier-options label").each(function(){$(this).removeClass("filter-selected");});
-				}
-			else
-				{
-				isAllTierOn = true;
-				$scope.Filter.Tier1			= 1;
-				$scope.Filter.Tier2			= 2;
-				$scope.Filter.Tier3			= 3;
-				$scope.Filter.Tier4			= 4;
-				$scope.Filter.Tier5			= 5;
-				$(".armors-tier-options label").each(function(){$(this).addClass("filter-selected");});
-				}
-			
-		};
-		
-		//toggle select all armorsmith
-		var isAllAsOn = false;
-		$scope.selectArmorAs = function(){
-			console.log("pushed"+isAllAsOn);
-			if(isAllAsOn)
-				{
-				isAllAsOn = false;
-				$scope.Filter.Bracers		= false;
-				$scope.Filter.Greaves		= false;
-				$scope.Filter.Helm			= false;
-				$scope.Filter.Chainmail	= false;
-				$scope.Filter.Gauntlets	= false;
-				$scope.Filter.Sabatons	= false;
-				$scope.Filter.GreatHelm	= false;
-				$scope.Filter.Platemail	= false;
-				$("#as-armors-subtype label").each(function(){$(this).removeClass("filter-selected");});
-				}
-			else
-				{
-				isAllAsOn = true;
-				$scope.Filter.Bracers		= "Bracers";
-				$scope.Filter.Greaves		= "Greaves";
-				$scope.Filter.Helm			= "Helm";
-				$scope.Filter.Chainmail	= "Chainmail";
-				$scope.Filter.Gauntlets	= "Gauntlets";
-				$scope.Filter.Sabatons	= "Sabatons";
-				$scope.Filter.GreatHelm	= "Great Helm";
-				$scope.Filter.Platemail	= "Platemail";
-				$("#as-armors-subtype label").each(function(){$(this).addClass("filter-selected");});
-				}
-		};
-		
-		//toggle select all clothworker
-		var isAllCwOn = false;
-		$scope.selectArmorCw = function(){
-			if(isAllCwOn)
-				{
-				isAllCwOn = false;
-				$scope.Filter.Gloves	= false;
-				$scope.Filter.Boots		= false;
-				$scope.Filter.Hood		= false;
-				$scope.Filter.Tunic		= false;
-				$scope.Filter.Mitts		= false;
-				$scope.Filter.Crakows	= false;
-				$scope.Filter.Hat			= false;
-				$scope.Filter.Robe		= false;
-				$("#cw-armors-subtype label").each(function(){$(this).removeClass("filter-selected");});
-				}
-			else
-				{
-				isAllCwOn = true;
-				$scope.Filter.Gloves	= "Gloves";
-				$scope.Filter.Boots		= "Boots";
-				$scope.Filter.Hood		= "Hood";
-				$scope.Filter.Tunic		= "Tunic";
-				$scope.Filter.Mitts		= "Mitts";
-				$scope.Filter.Crakows	= "Crakows";
-				$scope.Filter.Hat			= "Hat";
-				$scope.Filter.Robe		= "Robe";
-				$("#cw-armors-subtype label").each(function(){$(this).addClass("filter-selected");});
-				}
-		};	  
+	  
+		$(".filters-reset-button").on("click", function(item) {
+			$scope.Filter.Tier1		= false;
+			$scope.Filter.Tier2		= false;
+			$scope.Filter.Tier3		= false;
+			$scope.Filter.Tier4		= false;
+			$scope.Filter.Tier5		= false;
+			$scope.Filter.Bracers	= false;
+			$scope.Filter.Greaves	= false;
+			$scope.Filter.Helm		= false;
+			$scope.Filter.Chainmail	= false;
+			$scope.Filter.Gauntlets	= false;
+			$scope.Filter.Sabatons	= false;
+			$scope.Filter.GreatHelm	= false;
+			$scope.Filter.Platemail	= false;
+			$scope.Filter.Gloves	= false;
+			$scope.Filter.Boots		= false;
+			$scope.Filter.Hood		= false;
+			$scope.Filter.Tunic		= false;
+			$scope.Filter.Mitts		= false;
+			$scope.Filter.Crakows	= false;
+			$scope.Filter.Hat		= false;
+			$scope.Filter.Robe		= false;
+			$scope.$apply();
+			$(".filter-selected").removeClass("filter-selected");
+			$("#bs-weapons-subtypes").addClass("hide");
+			$("#ww-weapons-subtypes").addClass("hide");
+	  })
+	  
 	})
