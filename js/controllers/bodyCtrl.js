026bd7389a87cf6ba8e1a6bbc7ff186666c3e3ef
Phaturia <phaturia@gmail.com>
2016-02-12 10:44:28 +0100

    VersionUpdate
    stable: 1.83 -> 1.85
    beta:: 1.838 -> 1.850

--- js/controllers/bodyCtrl.js
+++ js/controllers/bodyCtrl.js
@@ -3,8 +3,8 @@ angular.module('mainApp')
 	.controller('bodyCtrl', function($scope, $http) {
 
 		//Game Version Numbers
-		$scope.betaVersion = "1.838";
-		$scope.liveVersion = "1.83";
+		$scope.betaVersion = "1.850";
+		$scope.liveVersion = "1.85";
 
 		//Side bar latest guides
 		$scope.guidesList = listOfGuides;
