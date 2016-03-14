fb3633c5263cf824a58a7eb0adcd73b34607da77
David Hughes <davehughesonline@gmail.com>
2016-03-14 17:42:13 -0600

    Added Sass folder and initial scss file to be complied into main.css.

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,27 @@ var concat = require('gulp-concat');
 var order = require("gulp-order");
 var uglify = require("gulp-uglify");
 var ngAnnotate = require('gulp-ng-annotate')
+var sass = require('gulp-sass');
 
 gulp.task('scripts', function() {
-  return gulp.src(['./js/**/*.js', '!./js/vendor/*.min.js', '!./js/modules/*.min.js'])
-  .pipe(order([
-    "main.js",
-    "directives/*.js",
-    "controllers/*.js",
-    "filters/*.js",
-    "others/*.js"
-  ]))
-  .pipe(concat('bundle.js'))
-  .pipe(ngAnnotate())
-  .pipe(uglify())
-  .pipe(gulp.dest('./js/'));
+  return gulp.src(['./js/**/*.js', '!./js/vendor/*.min.js', '!./js/modules/*.min.js', '!./js/bundle.js'])
+    .pipe(order([
+      "main.js",
+      "directives/*.js",
+      "controllers/*.js",
+      "filters/*.js",
+      "others/*.js"
+    ]))
+    .pipe(concat('bundle.js'))
+    .pipe(ngAnnotate())
+    .pipe(uglify())
+    .pipe(gulp.dest('./js/'));
 });
 
-gulp.task('default', ['scripts']);
+gulp.task('styles', function() {
+  gulp.src('sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('default', ['scripts', 'styles']);
