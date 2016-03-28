a6bc8d9fed7b80c7890d299538c31393f248901d
Phaturia <phaturia@gmail.com>
2016-03-28 18:39:32 +0200

    final drop chance fix

--- js/others/function.js
+++ js/others/function.js
@@ -530,8 +530,8 @@ function getMonsterByMaterialId(id) {
 		
 		if(odds)
 			{
-			var oddsMin = (100-val.nilOdds*1.4)/totalOdds*odds;
-			var oddsMax = (100-val.nilOdds*0.5)/totalOdds*odds;
+			var oddsMin = odds/(totalOdds+val.nilOdds*1.4)*100;
+			var oddsMax = odds/(totalOdds+val.nilOdds*0.5)*100;
 			var temp = {"name": i, "min":min, "max":max, "oddsMin":oddsMin, "oddsMax":oddsMax}
 			listDrops.push(temp);
 			}
