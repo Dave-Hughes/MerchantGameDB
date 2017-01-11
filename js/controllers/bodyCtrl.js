28f5824845ece64f5fe3bc1e86a27ac58ab7ec0d
István Kokics <phaturia@gmail.com>
2017-01-11 17:18:10 +0100

    version update
    update to version 2.2

--- js/controllers/bodyCtrl.js
+++ js/controllers/bodyCtrl.js
@@ -5,7 +5,7 @@ angular.module('mainApp')
 
 		// Game Version Numbers
 		$scope.betaVersion = "1.858";
-		$scope.liveVersion = "2.18";
+		$scope.liveVersion = "2.2";
 
 		// Logic for the "Latest Guides" sidebar widget
 		$scope.guidesList = listOfGuides;
