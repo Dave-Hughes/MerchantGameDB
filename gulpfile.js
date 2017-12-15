1465b9ae3147485970dec561dd49cf90ccce3883
Benzeliden <denis.krasakov@gmail.com>
2017-12-15 17:06:36 +0300

    Task for gulp to generate search.json

--- gulpfile.js
+++ gulpfile.js
@@ -28,6 +28,74 @@ gulp.task('styles', function() {
     .pipe(gulp.dest('./css/'));
 });
 
+gulp.task('regenerate-search', function(){
+  var fs = require('fs');
+  var basePath = "./json/stable/";
+  var equip = JSON.parse(fs.readFileSync(basePath + 'EquipmentList.json'));
+  var mats = JSON.parse(fs.readFileSync(basePath + 'MaterialList.json'));
+  var potions = JSON.parse(fs.readFileSync(basePath + 'PotionList.json'));
+  var quests = JSON.parse(fs.readFileSync(basePath + 'QuestList.json'));
+
+  function getItemType(itemSlot){
+    if (itemSlot == 1) return "Weapon";
+    if (itemSlot == 6) return "Trinket";
+    return "Armor";
+  }
+
+  var toSave = [];
+  for(var i = 0;i<equip.length;i++){
+    var item = equip[i];
+    if (item.itemSlot && item.image){
+      toSave.push({
+        name: item.name,
+        type: getItemType(item.itemSlot),
+        subType: item.subType,
+        rarity: item.rarity,
+        icon: item.image,
+      });
+    }
+  }
+  for(var i = 0;i<mats.length;i++){
+    var item = mats[i];
+    if (item.image && item.image.substr(-1) != "/"){
+      toSave.push({
+        name: item.name,
+        type: "Material",
+        subType: item.subType,
+        rarity: item.rarity,
+        icon: item.image,
+      });
+    }
+  }
+  for(var i = 0;i<potions.length;i++){
+    var item = potions[i];
+    if (item.image && item.image.substr(-1) != "/"){
+      toSave.push({
+        name: item.name,
+        type: "Potion",
+        subType: item.subType,
+        rarity: item.rarity,
+        icon: item.image,
+      });
+    }
+  }
+  
+  for(var i = 0;i<quests.length;i++){
+    var quest = quests[i];
+    if (quest.title != "Placeholder" && quest.image.substr(-1) != "/"){
+      toSave.push({
+        name: quest.name,
+        type: "Quest",
+        subType: quest.title || "Normal",
+        rarity: "1",
+        icon: quest.image,
+      });
+    }
+  }
+
+  fs.writeFileSync(basePath + "search.json", JSON.stringify(toSave));
+})
+
 gulp.task('default', ['scripts', 'styles']);
 
 gulp.task('dev', function(){
