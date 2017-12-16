3c46e2a72740a5b7b2fe6a85fb6d3f882546830f
Benzeliden <denis.krasakov@gmail.com>
2017-12-16 13:56:07 +0300

    Beta v 2.395. Removed duplicate dependencies in package.json

--- js/controllers/bodyCtrl.js
+++ js/controllers/bodyCtrl.js
@@ -4,7 +4,7 @@ angular.module('mainApp')
 	.controller('bodyCtrl', function($scope, $http) {
 
 		// Game Version Numbers
-		$scope.betaVersion = "2.389";
+		$scope.betaVersion = "2.395";
 		$scope.liveVersion = "2.25";
 
 		// Logic for the "Latest Guides" sidebar widget
