b1162b88eaba17411860129acc7cb123e8bd9e50
Benzeliden <denis.krasakov@gmail.com>
2017-12-26 19:03:36 +0300

    New db search. Added latest angucomplete-alt code.

--- js/controllers/searchController.js
+++ js/controllers/searchController.js
@@ -1,29 +1,29 @@
 //Controller for the search
 angular.module('mainApp')
-	.controller('searchController', function($scope, $http) {
+	.controller('searchController', function ($scope, searchService) {
 		$scope.gameObjects = jsonSearch;
 		//FOR WHEN A MENU ITEM IS CLICKED
-		$scope.itemSelected = function(selected) {
-		  if (selected) { //Need to know if on a DB page or not
-			var URLextender = ""
-			if(selected.originalObject.type == "Weapon")
-				{ URLextender = "items/weapon/" }
-			else if(selected.originalObject.type == "Armor")
-				{ URLextender = "items/armor/" }
-			else if(selected.originalObject.type == "Trinket")
-				{ URLextender = "items/trinket/" }
-			else if(selected.originalObject.type == "Material")
-				{ URLextender = "items/material/" }
-			else if(selected.originalObject.type == "Potion")
-				{ URLextender = "items/potion/" }
-			else
-				{ URLextender = "quests/" }
+		$scope.itemSelected = function (selected) {
+			if (selected) { //Need to know if on a DB page or not
+				var URLextender = ""
+				if (selected.originalObject.type == "Weapon") { URLextender = "items/weapon/" }
+				else if (selected.originalObject.type == "Armor") { URLextender = "items/armor/" }
+				else if (selected.originalObject.type == "Trinket") { URLextender = "items/trinket/" }
+				else if (selected.originalObject.type == "Material") { URLextender = "items/material/" }
+				else if (selected.originalObject.type == "Potion") { URLextender = "items/potion/" }
+				else { URLextender = "quests/" }
 
-			var objectURL = "#!/" + URLextender + selected.title;
-			$(location).attr('href', objectURL);
-			return false;
-		  } else {
-			console.log('cleared');
-		  }
+				var objectURL = "#!/" + URLextender + selected.title;
+				$(location).attr('href', objectURL);
+				return false;
+			} else {
+				console.log('cleared');
+			}
 		};
+
+		$scope.limitedSearch = function (str, items) {
+			var matches = searchService.fuseSearch(str).slice(0, 20);
+
+			return matches;
+		}
 	});
