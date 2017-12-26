be3cb660aebf87b9bd6f85bc4c4fa1e15f102198
Benzeliden <denis.krasakov@gmail.com>
2017-12-26 15:52:14 +0300

    Removed synchronous request for guides. Moved guides functionality to service

--- js/others/json.js
+++
@@ -1,9 +0,0 @@
-var listOfGuides = "";
-$.ajax({
-  url: "http://merchantgamedb.com/guides/wp-json/wp/v2/posts?filter[cat]=6",
-  dataType: 'json',
-  async: false,
-  success: function(data) {
-    listOfGuides = data;
-  }
-});
