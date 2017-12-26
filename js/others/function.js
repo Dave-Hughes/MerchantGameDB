d4f51bb96a8bb0380b53f4d417594561d171418b
Benzeliden <denis.krasakov@gmail.com>
2017-12-26 15:52:14 +0300

    Removed synchronous request for guides. Moved guides functionality to service

--- js/others/function.js
+++ js/others/function.js
@@ -991,37 +991,6 @@ function getPrefixStatByName(name) {
 	return stat;
 }
 
-function getGuideByTitle(title) {
-	 var guide = "";
-	 $.each(listOfGuides, function(index, val) {
-		 if (listOfGuides[index].title.rendered == title) {
-				guide = val;
-				return false;
-			}
-	 })
-	return guide;
-}
-
-function getAuthorByID(id) {
-	var author = "";
-	if (id == 1) {
-		author = "Chirp";
-	}
-	else if (id == 2) {
-		author = "Phaturia";
-	}
-	else if (id == 3) {
-		author = "EnzymSama";
-	}
-	else if (id == 4) {
-		author = "Kamighty";
-	}
-	else if (id == 5) {
-		author = "Rkinasz";
-	}
-	return author;
-}
-
 function trimDate(date) {
 	var length = 10;
 	var trimmedDate = date.substring(0, length);
