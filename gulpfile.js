ae42f028974ef80fe54165863bd4570a9dfd46e9
Benzeliden <denis.krasakov@gmail.com>
2017-12-27 14:00:05 +0300

    Css and json optimization. Disable minifying in development.

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,17 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var order = require("gulp-order");
 var uglify = require("gulp-uglify");
-var ngAnnotate = require('gulp-ng-annotate')
+var ngAnnotate = require('gulp-ng-annotate');
 var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+
+function checkDevelopment() {
+  //console.log("Node environment", process.env.NODE_ENV);
+  return process.env.NODE_ENV === 'dev';
+}
 
 gulp.task('scripts', function () {
-  return gulp.src(['./js/**/*.js', '!./js/vendor/*.min.js', '!./js/modules/*.min.js', '!./js/bundle.js', '!./js/jsonDB.js'])
+  var scriptsPipe = gulp.src(['./js/**/*.js', '!./js/vendor/*.min.js', '!./js/modules/*.min.js', '!./js/bundle.js', '!./js/jsonDB.js'])
     .pipe(order([
       "main.js",
       "directives/*.js",
@@ -17,20 +23,27 @@ gulp.task('scripts', function () {
       "others/*.js"
     ]))
     .pipe(concat('bundle.js'))
-    .pipe(ngAnnotate())
-    .pipe(uglify())
-    .pipe(gulp.dest('./js/'));
+    .pipe(ngAnnotate());
+
+  if (!checkDevelopment()) {
+    scriptsPipe = scriptsPipe.pipe(uglify());
+  }
+  return scriptsPipe.pipe(gulp.dest('./js/'));
 });
 
 gulp.task('styles', function () {
-  gulp.src('sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css/'));
+  var cssPipe = gulp.src('sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError));
+
+  if (!checkDevelopment()) {
+    cssPipe = cssPipe.pipe(cleanCSS());
+  }
+  return cssPipe.pipe(gulp.dest('./css/'));
 });
 
 gulp.task('regenerate-search', function () {
   var fs = require('fs');
-  var basePath = "./json/stable/";
+  var basePath = "./json/";
   var equip = JSON.parse(fs.readFileSync(basePath + 'EquipmentList.json'));
   var mats = JSON.parse(fs.readFileSync(basePath + 'MaterialList.json'));
   var potions = JSON.parse(fs.readFileSync(basePath + 'PotionList.json'));
@@ -42,10 +55,12 @@ gulp.task('regenerate-search', function () {
     return "Armor";
   }
 
-  var toSave = [];
+  var toSave = [], newEquip = [], newMats = [], newPotions = [], newQuests = [];
+
   for (var i = 0; i < equip.length; i++) {
     var item = equip[i];
     if (item.itemSlot && item.image) {
+      newEquip.push(item);
       toSave.push({
         name: item.name,
         type: getItemType(item.itemSlot),
@@ -53,11 +68,15 @@ gulp.task('regenerate-search', function () {
         rarity: item.rarity,
         icon: item.image,
       });
+    } else {
+      newEquip.push({name: item.name});//clean not existing items
     }
   }
   for (var i = 0; i < mats.length; i++) {
     var item = mats[i];
-    if (item.image && item.image.substr(-1) != "/") {
+    if (item.image && item.image.substr(-1) != "/" &&
+      item.image != "Materials/Region_6/Chieftains_Blade.png") {
+      newMats.push(item);
       toSave.push({
         name: item.name,
         type: "Material",
@@ -65,11 +84,14 @@ gulp.task('regenerate-search', function () {
         rarity: item.rarity,
         icon: item.image,
       });
+    } else {
+      newMats.push({name: item.name});//clean not existing items
     }
   }
   for (var i = 0; i < potions.length; i++) {
     var item = potions[i];
     if (item.image && item.image.substr(-1) != "/") {
+      newPotions.push(item);
       toSave.push({
         name: item.name,
         type: "Potion",
@@ -77,12 +99,15 @@ gulp.task('regenerate-search', function () {
         rarity: item.rarity,
         icon: item.image,
       });
+    } else {
+      newPotions.push({});//clean not existing items
     }
   }
 
   for (var i = 0; i < quests.length; i++) {
     var quest = quests[i];
     if (quest.title != "Placeholder" && quest.image.substr(-1) != "/") {
+      newQuests.push(quest);
       toSave.push({
         name: quest.name,
         type: "Quest",
@@ -90,10 +115,18 @@ gulp.task('regenerate-search', function () {
         rarity: "1",
         icon: quest.image,
       });
+    } else {
+      console.log("push placeholder")
+      newQuests.push({title: "Placeholder"});//clean not existing items
     }
   }
 
   fs.writeFileSync(basePath + "search.json", JSON.stringify(toSave));
+
+  fs.writeFileSync(basePath + "EquipmentList.json", JSON.stringify(newEquip));
+  fs.writeFileSync(basePath + "MaterialList.json", JSON.stringify(newMats));
+  fs.writeFileSync(basePath + "PotionList.json", JSON.stringify(newPotions));
+  fs.writeFileSync(basePath + "QuestList.json", JSON.stringify(newQuests));
 })
 
 gulp.task('pack-json', function () {
@@ -126,9 +159,11 @@ gulp.task('json', ["regenerate-search", "pack-json"]);
 
 gulp.task('default', ['scripts', 'styles']);
 
-gulp.task('dev', function () {
+gulp.task('dev-watch', function () {
   var liteServer = require("lite-server");
   liteServer.server();
-  gulp.watch(['./js/**/*.js', '!./js/vendor/*.min.js', '!./js/modules/*.min.js', '!./js/bundle.js'], ['scripts'])
-  gulp.watch('sass/**/*.scss', ['styles'])
-})
+  gulp.watch(['./js/**/*.js', '!./js/vendor/*.min.js', '!./js/modules/*.min.js', '!./js/bundle.js'], ['scripts']);
+  gulp.watch('sass/**/*.scss', ['styles']);
+});
+
+gulp.task('dev', ['default', 'dev-watch']);
